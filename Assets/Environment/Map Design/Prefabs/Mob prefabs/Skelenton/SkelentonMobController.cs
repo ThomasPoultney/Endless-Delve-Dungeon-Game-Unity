aab041fa0ce450819106ea6c9634c01a1bf1952b
ThomasPoultney <tompoultney@btinternet.com>
2021-11-20 01:03:40 +0000

    Changed Box collider size on skelenton weapon Swing

--- Assets/Environment/Map Design/Prefabs/Mob prefabs/Skelenton/SkelentonMobController.cs
+++ Assets/Environment/Map Design/Prefabs/Mob prefabs/Skelenton/SkelentonMobController.cs
@@ -1,12 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SkelentonMobController : MonoBehaviour
 {
-    public float walkSpeed = 20f;
-    public float horizontalMove = 0f;
-    
+    public float walkSpeed = 0.2f;
+
     public Animator animator;
     private bool isAttacking = false;
 
@@ -15,32 +12,35 @@ public class SkelentonMobController : MonoBehaviour
     private string currentAnimationState = "Skelenton_Idle";
     public LayerMask playerLayer;
     private Vector2 playerCheckDirection;
-
+    private BoxCollider2D boxCollider;
     // Start is called before the first frame update
     void Start()
     {
-
-
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         RaycastHit2D isOnPlatformEdge;
         Vector3 movingRotation;
-        
 
-        RaycastHit2D playerInMeleeRange = Physics2D.Raycast(transform.position, playerCheckDirection, 1f, playerLayer);
-        
+
+        RaycastHit2D playerInMeleeRange = Physics2D.Raycast(transform.position, playerCheckDirection, 0.5f, playerLayer);
+
 
         if (playerInMeleeRange.collider)
         {
+            
             ChangeAnimationState("Skelenton_Swing");
+            boxCollider.size = new Vector2(2, 1);
             isAttacking = true;
 
         }
         else
         {
+            boxCollider.size = new Vector2(1, 1);
+
             if (movingRight)
             {
                 transform.position += new Vector3(+walkSpeed, 0, 0);
@@ -52,7 +52,7 @@ public class SkelentonMobController : MonoBehaviour
             }
             else
             {
-               transform.position += new Vector3(-walkSpeed, 0, 0);
+                transform.position += new Vector3(-walkSpeed, 0, 0);
                 movingRotation = new Vector3(0, 0, 0);
                 isOnPlatformEdge = Physics2D.Raycast(transform.position + new Vector3(-1, 0, 0), Vector2.down, 2f);
                 ChangeAnimationState("Skelenton_Walk");
